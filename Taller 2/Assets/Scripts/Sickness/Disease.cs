14edcd7234a32f2d3740b8bc6601730706f3c87a
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-24 16:10:52 -0500

    a

--- Taller 2/Assets/Scripts/Sickness/Disease.cs
+++ Taller 2/Assets/Scripts/Sickness/Disease.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Disease : MonoBehaviour {
+public abstract class Disease : MonoBehaviour {
     [SerializeField] protected float onSet; //After this time Actor is going to show symptoms
     [SerializeField] protected float TimeUntilDeath; //After this time Actor will be dead;
 }
