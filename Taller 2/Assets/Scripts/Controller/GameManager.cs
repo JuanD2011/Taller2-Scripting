6a609e5ac90a18dd754536e797527c4f43a282d8
danialarcon0698 <daniel.alarcon@upb.edu.co>
2018-09-24 22:54:22 -0500

    Canvas

---
+++ Taller 2/Assets/Scripts/Controller/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public void PausedGame() {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
---
+++ Taller 2/Assets/Scripts/Controller/LvlMgr.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LvlMgr : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider slider;
+
+    public void Levels(string levelName)
+    {
+        StartCoroutine(LoadAsynchronously(levelName));
+    }
+
+    IEnumerator LoadAsynchronously(string _sceneName)
+    {
+        loadingScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_sceneName);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress * 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+}
